d0b01d196c40e63fb7a9fad365b8f8624b4606cb
unknown <WTRAGER@SDV-FQ4YN91.HQAD.MTAHQ.ORG>
2012-05-03 15:30:23 -0400

    changed logo href to mta.info

--- docroot/sites/all/themes/mta/page.tpl.php
+++ docroot/sites/all/themes/mta/page.tpl.php
@@ -11,7 +11,7 @@
 			endif; ?>
 			<div id="topbar">
 				<div id="branding">
-					<a href=""><img src="<?php print $base_path ?>sites/all/themes/mta/images/mta_info.gif"></a>
+					<a href="http://mta.info"><img src="<?php print $base_path ?>sites/all/themes/mta/images/mta_info.gif"></a>
 				</div>
 				<div id="middle-header">
 				<?php print render($page['header_middle']); ?>
