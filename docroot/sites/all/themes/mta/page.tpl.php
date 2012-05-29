8a275e2bb0b740f0c8dabeb3987136907ffbd202
unknown <WTRAGER@SDV-FQ4YN91.HQAD.MTAHQ.ORG>
2012-05-29 15:41:32 -0400

    continue clean up page tpl and base css

--- docroot/sites/all/themes/mta/page.tpl.php
+++ docroot/sites/all/themes/mta/page.tpl.php
@@ -28,7 +28,13 @@
 							<div class="container">
 								<div class="span-90">									
 									<?php print $messages; ?>
+			 				 <div id="pageTitleArea">
+									<?php print render($title_prefix); ?>
+									<?php if ($title): ?><h1 class="title" id="page-title"><?php print $title; ?></h1><?php endif; ?>
+									<?php print render($title_suffix); ?>
 									<?php print render($page['page_title']);?>
+									
+							</div>											
 									<?php if ($page['sidebar_first']): ?>
 										<div id="sidebar_first">
 											<?php print render($page['sidebar_first']); ?>
@@ -48,9 +54,8 @@
 								
 
 									<a id="main-content"></a>
-									<?php print render($title_prefix); ?>
-									<?php if ($title): ?><h1 class="title" id="page-title"><?php print $title; ?></h1><?php endif; ?>
-									<?php print render($title_suffix); ?>
+
+									
 									<?php if ($tabs): ?><div class="tabs"><?php print render($tabs); ?></div><?php endif; ?>
 									<?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
 									<?php print render($page['content']); ?>        
