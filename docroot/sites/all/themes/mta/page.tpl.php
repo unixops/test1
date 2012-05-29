41631773cdfa7045d186b1bdfa4ad0f7ae3d110b
unknown <WTRAGER@SDV-FQ4YN91.HQAD.MTAHQ.ORG>
2012-05-29 13:36:05 -0400

    add changes to page tpl and base css

--- docroot/sites/all/themes/mta/page.tpl.php
+++ docroot/sites/all/themes/mta/page.tpl.php
@@ -1,87 +1,12 @@
-<?php
-
-/**
- * @file
- * Default theme implementation to display a single Drupal page.
- *
- * Available variables:
- *
- * General utility variables:
- * - $base_path: The base URL path of the Drupal installation. At the very
- *   least, this will always default to /.
- * - $directory: The directory the template is located in, e.g. modules/system
- *   or themes/bartik.
- * - $is_front: TRUE if the current page is the front page.
- * - $logged_in: TRUE if the user is registered and signed in.
- * - $is_admin: TRUE if the user has permission to access administration pages.
- *
- * Site identity:
- * - $front_page: The URL of the front page. Use this instead of $base_path,
- *   when linking to the front page. This includes the language domain or
- *   prefix.
- * - $logo: The path to the logo image, as defined in theme configuration.
- * - $site_name: The name of the site, empty when display has been disabled
- *   in theme settings.
- * - $site_slogan: The slogan of the site, empty when display has been disabled
- *   in theme settings.
- *
- * Navigation:
- * - $main_menu (array): An array containing the Main menu links for the
- *   site, if they have been configured.
- * - $secondary_menu (array): An array containing the Secondary menu links for
- *   the site, if they have been configured.
- * - $breadcrumb: The breadcrumb trail for the current page.
- *
- * Page content (in order of occurrence in the default page.tpl.php):
- * - $title_prefix (array): An array containing additional output populated by
- *   modules, intended to be displayed in front of the main title tag that
- *   appears in the template.
- * - $title: The page title, for use in the actual HTML content.
- * - $title_suffix (array): An array containing additional output populated by
- *   modules, intended to be displayed after the main title tag that appears in
- *   the template.
- * - $messages: HTML for status and error messages. Should be displayed
- *   prominently.
- * - $tabs (array): Tabs linking to any sub-pages beneath the current page
- *   (e.g., the view and edit tabs when displaying a node).
- * - $action_links (array): Actions local to the page, such as 'Add menu' on the
- *   menu administration interface.
- * - $feed_icons: A string of all feed icons for the current page.
- * - $node: The node object, if there is an automatically-loaded node
- *   associated with the page, and the node ID is the second argument
- *   in the page's path (e.g. node/12345 and node/12345/revisions, but not
- *   comment/reply/12345).
- *
- * Regions:
- * - $page['help']: Dynamic help text, mostly for admin pages.
- * - $page['highlighted']: Items for the highlighted content region.
- * - $page['content']: The main content of the current page.
- * - $page['sidebar_first']: Items for the first sidebar.
- * - $page['sidebar_second']: Items for the second sidebar.
- * - $page['header']: Items for the header region.
- * - $page['footer']: Items for the footer region.
- *
- * @see template_preprocess()
- * @see template_preprocess_page()
- * @see template_process()
- */
-?>
 <meta http-equiv="X-UA-Compatible" content="IE=EmulateIE7"/>
 
 <!--[if lt IE 9]>
 			<style>.roundCorners, #block-google-appliance-ga-related-searches {border: 1px solid #b4b4b4;-moz-border-radius: 5px;-webkit-border-radius: 5px;border-radius: 5px;background: #fff;behavior: url(<?php print base_path();?>sites/all/themes/mta/border-radius.htc);}</style>
 		<![endif]-->
 		
-<div id="page-wrapper">
-	<div id="page">
+<div id="page">
 		<div id="mainbox">
-			<?php 
-			if ($secondary_sf_menu): ?>
-    	  		<div id="top-navigation"><div class="section clearfix">      
-        			<?php //print $secondary_sf_menu;  ?>
-				</div></div> <!-- /.section, /#top-navigation -->
-			    <?php 
-			endif; ?>
+
 			<div id="topbar">
 				<div id="branding">
 					<a href=""><img src="<?php print $base_path ?>sites/all/themes/mta/images/mta_info.gif"></a>
@@ -100,21 +25,16 @@
 								<?php if ($breadcrumb){print $breadcrumb;}?>
 			</div>
 			<div id="contentbox" class="roundCorners clearfix">
-				<div id="header">
-					<div class="clearfix">
-						<div id="logo-title">
 							<div class="container">
 								<div class="span-90">									
 									<?php print $messages; ?>
-									
-							<!-- 	<div class="span-23 pull-1 append-1"> -->	
+									<?php print render($page['page_title']);?>
 									<?php if ($page['sidebar_first']): ?>
 										<div id="sidebar_first">
 											<?php print render($page['sidebar_first']); ?>
 											</div>
 										<?php endif; ?>
 
-							<!-- 	</div>close span-23 -->
 								
 								
 								<?php if ($page['sidebar_first'] && $page['sidebar_second']) {
@@ -125,14 +45,12 @@
 										print'<div id="main-message" class="span-43 full-main">';
 								}
 								?>
+								
 
 									<a id="main-content"></a>
-									<div id="pageTitleArea" >
-										<?php print render($title_prefix); ?>
-										<?php if ($title): ?><h1 class="title" id="page-title"><?php print $title; ?></h1><?php endif; ?>
-										<span><?php print render($title_suffix); ?>
-										<?php print render($page['page_title']);?></span>
-									</div>
+									<?php print render($title_prefix); ?>
+									<?php if ($title): ?><h1 class="title" id="page-title"><?php print $title; ?></h1><?php endif; ?>
+									<?php print render($title_suffix); ?>
 									<?php if ($tabs): ?><div class="tabs"><?php print render($tabs); ?></div><?php endif; ?>
 									<?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
 									<?php print render($page['content']); ?>        
@@ -161,11 +79,8 @@
 
 								<!-- close span-21 last -->
 						</div><!-- close container for grid -->
-					</div>
-				</div>
 			</div>
-		</div>
+
 	</div>
-	<?php print render($page['footer']);?>
-	</div>
-	</div>
+		<?php print render($page['footer']);?>	
+</div>
