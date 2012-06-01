9f75e642a9132699c30ff32e52201c249cf1c377
unknown <WTRAGER@SDV-FQ4YN91.HQAD.MTAHQ.ORG>
2012-06-01 12:44:36 -0400

    remove dsm from page_with_ad.tpl

--- docroot/sites/all/themes/mta/page--ad.tpl.php
+++ docroot/sites/all/themes/mta/page--ad.tpl.php
@@ -31,7 +31,7 @@
 									<?php print render($title_prefix); ?>
 									<?php if ($title): ?><h1 class="title" id="page-title"><?php print $title; ?></h1><?php endif; ?>
 									<?php print render($title_suffix); ?>
-									<?php print render($page['page_title']); dsm('here is page_title '.$page['page_title']);?>
+									<?php print render($page['page_title']);?>
 									
 							</div>											
 
