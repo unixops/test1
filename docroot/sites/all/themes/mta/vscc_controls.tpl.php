7750f08348e27b543ff6f060dc4ae34ce30584c9
unknown <DANDREAN@SDV-CQ4YN91.HQAD.MTAHQ.ORG>
2012-08-22 14:38:28 -0400

    news rotator controls

--- docroot/sites/all/themes/mta/vscc_controls.tpl.php
+++ docroot/sites/all/themes/mta/vscc_controls.tpl.php
@@ -5,13 +5,12 @@
  */
 ?>
 
-<div id="vscc_controls_<?php print $variables['vss_id']; ?>"
-	class="<?php print $classes; ?>">
-	<?php print $rendered_control_previous; ?>
-	<?php if (isset($rendered_control_pause)) {
-		print $rendered_control_pause;
+<div id="vscc_controls_<?php print $variables['vss_id']; ?>" class="<?php print $classes; ?>">
+  <?php print $rendered_control_previous; ?>
+  <?php if (isset($rendered_control_pause)) {
+    print $rendered_control_pause;
   } ?>
-	<?php print $rendered_control_next; ?>
+  <?php print $rendered_control_next; ?>
 </div>
 
 
