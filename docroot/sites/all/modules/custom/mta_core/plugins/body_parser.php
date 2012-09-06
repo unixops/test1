5bfe1bcd0694f517c8dec44e4bba6b02b2cdf988
U-HQAD\WTRAGER <WTRAGER@SDV-FQ4YN91.HQAD.MTAHQ.ORG>
2012-09-06 09:29:09 -0400

    mod body_parser.php

--- docroot/sites/all/modules/custom/mta_core/plugins/body_parser.php
+++ docroot/sites/all/modules/custom/mta_core/plugins/body_parser.php
@@ -6,10 +6,10 @@
  */
 
 $plugin = array(
-  'form' => 'mta_core_body_parse_form',
-  'callback' => 'mta_core_body_parse_callback',
-  'validate' => 'mta_core_body_parse_validate',
-  'name' => 'Date Parse',
+  'form' => 'mta_core_body_parser_form',
+  'callback' => 'mta_core_body_parser_callback',
+  'validate' => 'mta_core_body_parser_validate',
+  'name' => 'Body Parser',
   'multi' => 'skip',
   'category' => 'Text',
 );
@@ -18,25 +18,26 @@ function mta_core_body_parse_form($importer, $element_key, $settings) {
   $form = array();
   $form['body_format'] = array(
       '#type' => 'textfield',
-      '#title' => t('Date Format'),
+      '#title' => t('Body Processing'),
       '#default_value' => isset($settings['body_format']) ? $settings['body_format'] : '',
-      '#description' => t("The formating of the incoming date string, i.e. mdy, as per " . l('Php.net', 'http://us.php.net/manual/en/function.date.php') . ".
-          It's especially useful if the incoming date isn't in an English date format."),
+      '#description' => t("We look for file links and upload their contents to the server and re-write the link to its new home"),
   );
   return $form;
 }
 
 function mta_core_body_parse_validate(&$settings) {
   if (empty($settings['body_format'])) {
-    form_set_error('settings][body_format', t('Date format must not be empty.'));
+    form_set_error('settings][body_format', t('Body must not be empty.'));
   }
 }
 
 function mta_core_body_parse_callback($result, $item_key, $element_key, &$field, $settings) {
   if($field) {
-    $date = DateTime::createFromFormat('ymd', $field);
+    // enter parse code here
+    dsm($field);
+    
   } else {
-    $date = DateTime::createFromFormat('ymd', '100201');
+
   }
-  $field = $date->format('Y-m-d');
+  
 }
