099665a5ee94029646f65713b2cd1b43e33c73ea
U-HQAD\WTRAGER <WTRAGER@SDV-FQ4YN91.HQAD.MTAHQ.ORG>
2012-09-17 12:46:01 -0400

    change rotator preview

--- docroot/sites/all/themes/mta/node--news_article.tpl.php
+++ docroot/sites/all/themes/mta/node--news_article.tpl.php
@@ -15,7 +15,16 @@
     </div>
   <?php endif; ?>
 
-<p>&nbsp;</p>
+
+  <div class="content"<?php print $content_attributes; ?>>
+    <?php
+      // We hide the comments and links now so that we can render them later.
+      hide($content['comments']);
+      hide($content['links']);
+      print render($content);
+    ?>
+  </div>
+  <p>&nbsp;</p>
 <p>Here is a preview of this News Article's rotator slide.
 </p>
 <div id="rotator-preview">
@@ -29,14 +38,6 @@
 </div>
 <p>&nbsp;</p>
 <p>&nbsp;</p>
-  <div class="content"<?php print $content_attributes; ?>>
-    <?php
-      // We hide the comments and links now so that we can render them later.
-      hide($content['comments']);
-      hide($content['links']);
-      print render($content);
-    ?>
-  </div>
 
   <?php print render($content['links']); ?>
 
