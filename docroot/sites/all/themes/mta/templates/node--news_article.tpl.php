190d4706171ba0590c27a66e85404163bdbc674e
Wes Roepken <wroepken@mtahq.org>
2013-01-09 13:51:01 -0500

    Added if around intermittant error foreach

--- docroot/sites/all/themes/mta/templates/node--news_article.tpl.php
+++ docroot/sites/all/themes/mta/templates/node--news_article.tpl.php
@@ -16,7 +16,7 @@
   <?php endif; ?>
 
   <?php print render($content['links']); ?>
-<?php 
+<?php
 global $user;
 $check = array_intersect(array('master content provider', 'content provider', 'administrator'), array_values($user->roles));
 ?>
@@ -25,16 +25,16 @@ $check = array_intersect(array('master content provider', 'content provider', 'a
 	<p>Here is a preview of this News Article's rotator slide.
 	</p>
 	<div id="rotator-preview">
-	
+
 	<?php    // is of admin type person
 	   //  print views_embed_view('news_rotator_preview', 'block_1', $node->nid); // with single argument
 	?>
 	</div>
-	
+
 	<div class="view-news-rotator-preview">
 	<div class="views_slideshow_main">
 	<div id="views_slideshow_cycle_div_news_rotator_preview-block_1_0" class="news-rot-image-large">
-  <?php 
+  <?php
   if ($node->field_news_rotator_style['und'][0]['value']==0){
 		print theme('image_style', array('style_name' => 'wide_rotator', 'path' => $node->field_wide_image['und'][0]['uri']));
 	  print '<div class="news-rot-image-caption">';
@@ -42,7 +42,7 @@ $check = array_intersect(array('master content provider', 'content provider', 'a
 	  print $node->field_image_caption['und'][0]['value'];
 	  print '</div>';
 	  print '</div>';
-  
+
   }elseif ($node->field_news_rotator_style['und'][0]['value']==3){
     print '<div class="news-rot-portrait-preview">'.theme('image_style', array('style_name' => 'portrait', 'path' => $node->field_portrait['und'][0]['uri'])).'</div>';
     print '<div class="news-rot-image-caption">';
@@ -50,24 +50,24 @@ $check = array_intersect(array('master content provider', 'content provider', 'a
     print $node->field_image_caption['und'][0]['value'];
     print '</div>';
     print '</div>';
-  
+
   }elseif ($node->field_news_rotator_style['und'][0]['value']==1){
   	print '<div><h2>'.$node->field_head_title['und'][0]['value'].'</h2></div>';
      print '<div class="news-rot-image">'.theme('image_style', array('style_name' => 'medium', 'path' => $node->field_image['und'][0]['uri'])).'</div>';
     print '<div class="new-rot-title" style="width:200px; color:#0055AA;">'.$node->field_headline['und'][0]['value'].'</div>';
     print '<div class="news-rot-teaser" style="width:200px;">'.$node->field_article_summary['und'][0]['value'].'</div>';
-    
+
   }elseif ($node->field_news_rotator_style['und'][0]['value']==2){
      print '<div class="emergency-rot-title">'.$node->field_emergency_headline['und'][0]['value'].'</div>';
      print '<div>'.$node->field_emergency_summary['und'][0]['value'].'</div>';
   }
 	?>
-	
+
 
 	</div>
 	</div>
 	</div>
-	
+
 	<p>&nbsp;</p>
 	<p>&nbsp;</p>
 
@@ -86,10 +86,10 @@ $check = array_intersect(array('master content provider', 'content provider', 'a
 			print theme('image_style', array('style_name' => 'medium', 'path' => $node->field_image['und'][0]['uri']));
 			print '<div style="width:220px; margin-top:0;">'.$node->field_image['und'][0]['title'].'</div>';
 		}
-				
+
 			?>
 	</div>
-	
+
 	<?php //print views_embed_view('mta_news', 'page_1', $node->nid);?>
 	<?php print $node->field_article_body['und'][0]['value'];?>
 	<div>
@@ -101,7 +101,7 @@ $check = array_intersect(array('master content provider', 'content provider', 'a
 	<?php print $node->field_article_body['und'][0]['value'];?>
 	<div class = "first-picture-multi">
 	<?php ?>
-	<?php 
+	<?php
 	  if ($node->field_news_rotator_style['und'][0]['value']==0){
 	    print theme('image_style', array('style_name' => 'medium', 'path' => $node->field_wide_image['und'][0]['uri']));
 	  	print '<div style="width:220px; margin-top:0;">'.$node->field_wide_image['und'][0]['title'].'</div>';
@@ -116,20 +116,21 @@ $check = array_intersect(array('master content provider', 'content provider', 'a
 	  }
 	  ?>
 	</div>
-	<?php 
+	<?php
 		print '<div class="more-pictures-multi">';
-	
-	foreach ($field_mta_image['und'] as $more_images) {
-		print '<div class="more-images-multi">';
-		print theme('image_style', array('style_name' => 'medium', 'path' => $more_images['uri']));
-		print $more_images['title'];
+	if($field_mta_image['und']){
+  	foreach ($field_mta_image['und'] as $more_images) {
+  		print '<div class="more-images-multi">';
+  		print theme('image_style', array('style_name' => 'medium', 'path' => $more_images['uri']));
+  		print $more_images['title'];
+  		print '</div>';
+  	}
+  }
 		print '</div>';
-	}
-		print '</div>';
-	
+
 	?>
-	
-	
+
+
 	<?php //print views_embed_view('mta_news', 'page_2', $node->nid);?>
 <?php endif;?>
 
@@ -141,7 +142,7 @@ $check = array_intersect(array('master content provider', 'content provider', 'a
       hide($content['comments']);
       hide($content['links']);
       print render($content);
-      
+
     ?>
   </div>
 
