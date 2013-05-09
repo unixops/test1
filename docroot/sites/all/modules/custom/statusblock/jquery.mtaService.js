a2ee5811c9dbe51d40e1a4c93257091c9d914ba9
Kamal Sheikh <kamal.sheikh@icaremanager.com>
2013-05-09 13:57:43 -0400

    First fully working prototype

---
+++ docroot/sites/all/modules/custom/statusblock/jquery.mtaService.js
@@ -0,0 +1,44 @@
+/*!
+ * MTA Service Status Plugin
+ */
+ (function($) {
+
+    $.fn.mtaService = function(service, line) {
+
+        var ele = $(this);
+
+        url = "http://newmta.localhost:8082/service_status_json";
+
+        $.getJSON(url,
+            function(data){  
+                var arr;
+
+                $.ajaxSetup({ cache: false });
+
+                // console.log("This is: " + this);
+                // ele.html (data.subway.line[3].text);
+                // console.log (data.subway.line[3].text);
+
+                $.each(data, function(index, d){
+                    // console.log (data.timestamp);
+                    if (service == "subway")
+                        arr = data.subway;
+                    else if (service == "LIRR")
+                        arr = data.LIRR;
+
+                    $.each(arr, function(key, val){
+                        $.each(val, function(index, v){
+                            if (v.name == line)
+                            {
+                                console.log(v.status);
+                                console.log(v.name);
+                                ele.html(v.text);
+                                return false;
+                            }
+                        });
+                    });
+                });
+            }
+        );
+    }
+}(jQuery));
---
+++ docroot/sites/all/modules/custom/statusblock/ss.js
@@ -0,0 +1,62 @@
+/*!
+ * MTA Service Status Plugin
+ */
+ (function($) {
+
+    $().ready(function(){
+
+        $('#pageTitleArea').html("<h2>MTA Service Status</h2>");
+
+        url = "http://newmta.localhost:8082/service_status_json";
+        
+        $.getJSON(url,
+            function(data){  
+                
+                $.ajaxSetup({ cache: false });
+
+                $.each(data, function(index, d){
+                    console.log (data.subway.line[3].name);
+                    $.each(data.subway, function(key, val){
+                        $.each(val, function(index, val){
+                            // console.log(key);
+                            // console.log(val.name);
+                            generateHTML(val.name, val.status);
+                        });
+                    });
+                });
+            });
+        });
+
+    function generateHTML(name, val)
+    {
+        switch(val) {
+            case 'GOOD SERVICE':
+                $('#'+name).addClass("subway_GoodService");
+                $('#'+name).html(val);
+                break;
+            case 'PLANNED WORK':
+                $('#'+name).addClass("subway_PlannedWork");
+                var html = '<a style="color: #996600" href="/serviceStatus/subway/' + name + '">Planned Work</a>';
+                $('#'+name).html(html);
+                break;
+            case 'SERVICE CHANGE':
+                $('#'+name).addClass("subway_ServiceChange");
+                var html = '<a style="color: #996600" href="/serviceStatus/subway/' + name + '">Service Change</a>';
+                $('#'+name).html(html);
+                break;
+            case 'DELAYS':
+                $('#'+name).addClass("subway_Delays");
+                var html = '<a style="color: #990033" href="/serviceStatus/subway/' + name + '">Delays</a>';
+                $('#'+name).html(html);
+                break;
+            case 'SANDY REROUTE':
+                $('#'+name).addClass("subway_sandyreroute");
+                var html = '<a style="color: #5d0dff" href="/serviceStatus/subway/' + name + '">Sandy Reroute</a>';
+                $('#'+name).html(html);
+                break;
+        }
+
+        return html;
+    }
+
+}(jQuery));
