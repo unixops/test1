ce25af635644084a9849751d837277202ede13c5
Jim Wilde <jwilde@mtahq.org>
2013-07-03 12:54:59 -0400

    adding new module to help with css/js aggregation and caching

---
+++ docroot/sites/all/modules/contrib/advagg/advagg.admin.js
@@ -0,0 +1,38 @@
+
+/**
+ * @file
+ * Used to toggle the AdvAgg Bypass Cookie client side.
+ */
+
+/**
+ * Toggle the advagg cookie
+ *
+ * @return
+ *   FALSE so the form does not get submitted.
+ */
+function advagg_toggle_cookie() {
+  var cookie_name = 'AdvAggDisabled';
+
+  // See if the cookie exists.
+  var cookie_pos = document.cookie.indexOf(cookie_name + '=' + Drupal.settings.advagg.key);
+
+  // If the cookie does exist then remove it.
+  if (cookie_pos !== -1) {
+    document.cookie = cookie_name + '=;'
+      + 'expires=-1;'
+      + ' path=' + Drupal.settings.basePath + ';'
+      + ' domain=.' + document.location.hostname + ';';
+    alert(Drupal.t('AdvAgg Bypass Cookie Removed'));
+  }
+  // If the cookie does not exist then set it.
+  else {
+    document.cookie = cookie_name + '=' + Drupal.settings.advagg.key + ';'
+      + ' path=' + Drupal.settings.basePath + ';'
+      + ' domain=.' + document.location.hostname + ';';
+    alert(Drupal.t('AdvAgg Bypass Cookie Set'));
+  }
+
+  // Must return false, if returning true then form gets submitted.
+  return false;
+}
+
