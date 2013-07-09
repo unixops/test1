69feb99ef0db24f9ac737c876317dfc29c2bb948
Jim Wilde <jwilde@mtahq.org>
2013-07-09 13:30:10 -0400

    update global css for superfish menu

--- docroot/sites/default/settings.php
+++ docroot/sites/default/settings.php
@@ -495,7 +495,7 @@ ini_set('display_startup_errors', 0);
  *
  * Add leading hash signs if you would like to disable this functionality.
  */
-$conf['404_fast_paths_exclude'] = '/\/(?:styles)\//';
+$conf['404_fast_paths_exclude'] = '/\/(?:styles|advagg_(cs|j)s)\//';
 $conf['404_fast_paths'] = '/\.(?:txt|png|gif|jpe?g|css|js|ico|swf|flv|cgi|bat|pl|dll|exe|asp)$/i';
 $conf['404_fast_html'] = '<html xmlns="http://www.w3.org/1999/xhtml"><head><title>404 Not Found</title></head><body><h1>Not Found</h1><p>The requested URL "@path" was not found on this server.</p></body></html>';
 
