17477a595808caff1f781f7aecea997fdca162ec
Jim Wilde <jwilde@mtahq.org>
2013-08-27 11:25:03 -0400

    fix memcache settings

--- docroot/sites/default/settings.php
+++ docroot/sites/default/settings.php
@@ -555,4 +555,3 @@ if (file_exists('/var/www/site-php')) {
 $conf['cache_backends'][] = './sites/all/modules/contrib/memcache/memcache.inc';
 $conf['cache_default_class'] = 'MemCacheDrupal';
 $conf['cache_class_cache_form'] = 'DrupalDatabaseCache';
-/
